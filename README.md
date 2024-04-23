7dc988fffcfb60f20d42c05e808f9dc0a498ad21
Rashad Edwards <rashadedwards@macbook.local>
2024-04-23 00:33:05 -0400

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # emotionRate
+
+-- Change the font, review functions and names before moving on. Figure out your color gradients dont over think it. Find an animation you will like to use as well.
--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const circles = document.querySelectorAll(".circles");
 const emogiContent = document.querySelectorAll("#emoji-content");
 const emojiImg = document.querySelectorAll("#insideBox");
+const feelingDisplay = document.querySelectorAll("#circle-l");
 
 const happyArr = [
   {
     emotion: "Happy",
-    quote: '"Stay Postive in the mist of cahos"',
+    quote: '"Stay postive in the mist of cahos"',
   },
   {
-    emotion: "happy",
-    quote: "I am happy2",
+    emotion: "Happy",
+    quote: '"Learn to value yourself!"',
+  },
+  {
+    emotion: "Happy",
+    quote: '"Whoever is happy will make others happy!"',
+  },
+  {
+    emotion: "Happy",
+    quote: '"Happiness is only real when shared!"',
+  },
+  {
+    emotion: "Happy",
+    quote: '"Be happy it drives people crazy!"',
+  },
+  {
+    emotion: "Happy",
+    quote: '"Smile while you still have teeth!"',
   },
 ];
 const sadArr = [
@@ -21,15 +38,31 @@ const sadArr = [
     emotion: "sad",
     quote: "sadness two",
   },
+  {
+    emotion: "sad",
+    quote: "sadness three",
+  },
 ];
+
+function feelinTitle() {
+  for (i = 0; i < feelingDisplay.length; i++) {
+    feelingDisplay[0].style.backgroundColor = "red";
+    feelingDisplay[1].style.backgroundColor = "blue";
+    feelingDisplay[2].style.backgroundColor = "purple";
+    feelingDisplay[3].style.backgroundColor = "orange";
+    feelingDisplay[4].style.backgroundColor = "pink";
+    feelingDisplay[5].style.backgroundColor = "green";
+  }
+}
 
 function randomCircles() {
   for (i = 0; i < circles.length; i++) {
     // index circle three
     circles[i].style.display = "flex";
     circles[i].style.display = "space-between";
-    circles[i].style.width = "150px";
-    circles[i].style.height = "150px";
+    circles[i].style.padding = "5px";
+    circles[i].style.width = "165px";
+    circles[i].style.height = "165px";
     circles[i].style.borderRadius = "150px";
   }
 }
@@ -64,19 +97,14 @@ function displayTextFormat() {
 
 function displayHappyText() {
   for (i = 0; i < circles.length; i++) {
+    randomCircles(); // change name of functions
     displayTextFormat();
-    randomCircles();
+    mouseMoveHappy();
+    mouseOutHappy();
 
-    circles[0].style.backgroundColor = "blue";
     circles[0].textContent = happyArr[0].emotion;
-
-    circles[0].addEventListener("mousemove", () => {
-      circles[0].textContent = happyArr[0].quote;
-    });
-
-    circles[0].addEventListener("mouseout", () => {
-      circles[0].textContent = happyArr[0].emotion;
-    });
+    circles[1].textContent = happyArr[1].emotion;
+    circles[2].textContent = happyArr[2].emotion;
   }
 }
 
@@ -84,13 +112,80 @@ function displaySadText() {
   for (i = 0; i < circles.length; i++) {
     displayTextFormat();
     randomCircles();
-    circles[0].textContent = sadArr[0].quote;
-    circles[0].style.backgroundColor = "red";
-    circles[1].textContent = sadArr[1].quote;
+    mouseMoveSad();
+    mouseOutSad();
+    circles[0].textContent = sadArr[0].emotion;
+    circles[1].textContent = sadArr[1].emotion;
+    circles[2].textContent = sadArr[2].emotion;
+  }
+}
+
+function mouseMoveHappy() {
+  for (i = 0; i < circles.length; i++) {
+    // Index 0 happyArr Quote
+    circles[0].addEventListener("mousemove", () => {
+      circles[0].textContent = happyArr[0].quote;
+    });
+
+    circles[1].addEventListener("mousemove", () => {
+      circles[1].textContent = happyArr[1].quote;
+    });
+
+    circles[2].addEventListener("mousemove", () => {
+      circles[2].textContent = happyArr[2].quote;
+    });
+  }
+}
+
+function mouseOutHappy() {
+  for (i = 0; i < circles.length; i++) {
+    // Index 0 happyArr Quote
+    circles[0].addEventListener("mouseout", () => {
+      circles[0].textContent = happyArr[0].emotion;
+    });
+
+    circles[1].addEventListener("mouseout", () => {
+      circles[1].textContent = happyArr[1].emotion;
+    });
+    circles[2].addEventListener("mouseout", () => {
+      circles[2].textContent = happyArr[2].emotion;
+    });
+  }
+}
+
+function mouseMoveSad() {
+  for (i = 0; i < circles.length; i++) {
+    // Index 0 happyArr Quote
+    circles[0].addEventListener("mousemove", () => {
+      circles[0].textContent = sadArr[0].emotion;
+    });
+
+    circles[1].addEventListener("mousemove", () => {
+      circles[1].textContent = sadArr[1].emotion;
+    });
+
+    circles[2].addEventListener("mousemove", () => {
+      circles[2].textContent = sadArr[2].emotion;
+    });
+  }
+}
+function mouseOutSad() {
+  for (i = 0; i < circles.length; i++) {
+    // Index 0 happyArr Quote
+    circles[0].addEventListener("mouseout", () => {
+      circles[0].textContent = sadArr[0].quote;
+    });
+
+    circles[1].addEventListener("mouseout", () => {
+      circles[1].textContent = sadArr[1].quote;
+    });
+
+    circles[2].addEventListener("mouseout", () => {
+      circles[2].textContent = sadArr[2].quote;
+    });
   }
 }
 
 emojiClick();
 emojiImgClick();
-
-// displayTextInCircle();
+feelinTitle();
